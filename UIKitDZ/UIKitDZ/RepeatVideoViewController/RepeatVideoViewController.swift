5098780a5eabe346adbf11db3217d1a020bc098f
Anna Sycheva <anna.sycheva@cleverpumpkin.ru>
2022-09-28 11:14:10 +0300

    add pickerView

--- UIKitDZ/UIKitDZ/RepeatVideoViewController/RepeatVideoViewController.swift
+++ UIKitDZ/UIKitDZ/RepeatVideoViewController/RepeatVideoViewController.swift
@@ -12,11 +12,15 @@ final class RepeatVideoViewController: UIViewController, UITextFieldDelegate {
     var buttonShare = UIButton()
     var textField = UITextField()
     var activityViewController: UIActivityViewController?
-
+    var something = ["one", "two", "three"]
+    
+    @IBOutlet weak var somethingPickerView: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         createTextField()
         createButton()
+        setupPickerView()
     }
     
     // MARK: - Method
@@ -35,6 +39,11 @@ final class RepeatVideoViewController: UIViewController, UITextFieldDelegate {
         buttonShare.setTitle("Расшарить", for: .normal)
         buttonShare.addTarget(self, action: #selector(hendelShare), for: .touchUpInside)
         view.addSubview(buttonShare)
+    }
+    
+    func setupPickerView() {
+        somethingPickerView.delegate = self
+        somethingPickerView.dataSource = self
     }
     
     @objc func hendelShare(paramSender: Any) {
@@ -60,5 +69,33 @@ final class RepeatVideoViewController: UIViewController, UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
+    }
+}
+
+// MARK: - UIPickerViewDelegate, UIPickerViewDataSource
+
+extension RepeatVideoViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return something.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return something[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if row == 2 {
+            activityViewController = UIActivityViewController(
+                activityItems: [something[row]],
+                applicationActivities: nil
+            )
+            guard let activityViewController = activityViewController else { return }
+            present(activityViewController, animated: true)
+        }
     }
 }
